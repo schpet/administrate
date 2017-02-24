87400b31fb8e8e361a7c8ad12e95815e7b9803b8
Peter Schilling <peter@peterschilling.org>
2017-02-23 19:18:40 -0800

    Add Time field

--- spec/example_app/db/schema.rb
+++ spec/example_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170224010823) do
+ActiveRecord::Schema.define(version: 20170224024549) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20170224010823) do
     t.boolean  "email_subscriber"
     t.string   "kind",             default: "standard", null: false
     t.date     "birthdate"
+    t.time     "example_time"
   end
 
   create_table "delayed_jobs", force: :cascade do |t|
